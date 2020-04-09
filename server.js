2212094e4d0a61701919ba027d3bff63ee23bd06
BrandedNomad <info@brandednomad.com>
2020-04-09 18:32:35 +0700

    initial.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const server = express();
+const port = process.env.port || 3000;
+
+server.get('/',(req,res)=>{
+    res.send('You Won!')
+});
+
+server.listen(port,()=>{
+    console.log('Port is running on ' + port);
+});
+
+
+
+
+
+
