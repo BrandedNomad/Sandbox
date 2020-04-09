729d1009b24716193fc407cd170548fa955999d0
BrandedNomad <info@brandednomad.com>
2020-04-09 18:40:40 +0700

    fixed PORT.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const server = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.get('/',(req,res)=>{
     res.send('You Won!')
